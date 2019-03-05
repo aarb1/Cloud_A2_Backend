81d08a92796400c83d17c924f934c4c1e361875b
Leo Zhang <zzhan463.uwo.ca>
2019-03-04 19:23:15 -0500

    updating allVMUsage

--- monitor.js
+++ monitor.js
@@ -21,9 +21,9 @@ class Event {
 
 
 const fetch = require("node-fetch");
-const vmTypes = ["Basic", "Large", "Ultra-Large"]
+const vmTypes = ["Basic", "Large", "Ultra-Large"];
 
-const monitor = module.exports = {}
+const monitor = module.exports = {};
 
 smartFetch = (url, arguments) =>{
     return new Promise((resolve, reject) =>{
@@ -37,57 +37,57 @@ smartFetch = (url, arguments) =>{
                 reject(e)
             })
     })
-}
+};
 
-let rootURL = "https://clouda2backend.firebaseio.com"
+let rootURL = "https://clouda2backend.firebaseio.com";
 
 postEvent = (postData, url) =>{
     return new Promise ((resolve) =>{
         let arguments = {
             method: "POST",
             body: JSON.stringify(postData)
-        }
+        };
         smartFetch(url, arguments).then(data =>{
             resolve(data)
         }).catch(e =>{
             console.log(e)
         })
     })
-}
+};
 
 /**
  * Creates a VM for the specified user and returns {success, vmID, and ccID}
  */
 monitor.createVM = (Event) => {
     return new Promise((resolve) => {
-        let url = `${rootURL}/${Event.ccID}.json`
+        let url = `${rootURL}/${Event.ccID}.json`;
 
-        let body = Event
+        let body = Event;
 
-        body.events = {}
+        body.events = {};
 
         //Instantiate the VM's events with a "Create" event.
         body.events["-000000"] = {
             eventTime: Event.eventTime,
             eventType: "Create"
-        }
+        };
 
         //Remove extraneous data properties
-        delete body.eventType
-        delete body.eventTime
-        delete body.vmID
-        delete body.ccID
+        delete body.eventType;
+        delete body.eventTime;
+        delete body.vmID;
+        delete body.ccID;
 
         postEvent(body, url).then(data => {
             let response = {
                 success: true,
                 vmID: data.name, //firebase returns this as a name
                 ccID: Event.ccID
-            }
+            };
             resolve(response)
         })
     })
-}
+};
 
 /**
  * Start, Stop, Upgrade, Downgrade, Delete
@@ -95,24 +95,24 @@ monitor.createVM = (Event) => {
  */
 monitor.event = (Event) => {
     return new Promise((resolve) => {
-        let url = `${rootURL}/${Event.ccID}/${Event.vmID}/events.json`
-        let body = Event
+        let url = `${rootURL}/${Event.ccID}/${Event.vmID}/events.json`;
+        let body = Event;
 
         //Remove extraneous data properties
-        delete body.vmType
-        delete body.vmID
-        delete body.ccID
+        delete body.vmType;
+        delete body.vmID;
+        delete body.ccID;
 
         postEvent(body, url).then(data => {
             let response = {
                 success: true,
                 vmID: Event.vmID, //firebase returns this as a name
                 ccID: Event.ccID
-            }
+            };
             resolve(response)
         })
     })
-}
+};
 
 // getVms returns an object, where each key is a VM and contains the current vmType (calculated)
 // as well as the list of events that have ocurred
@@ -137,32 +137,32 @@ monitor.event = (Event) => {
 
 monitor.getVMs = (Event) => {
     return new Promise((resolve) => {
-        let url = `${rootURL}/${Event.ccID}.json`
+        let url = `${rootURL}/${Event.ccID}.json`;
         let arguments = {
             method: "GET",
-        }
+        };
         smartFetch(url, arguments).then(data =>{
             if (data !== null){
 
                 Object.keys(data).forEach(key =>{
                     //Flatten events into arrays for eacn virtual machine
-                    let vm = data[key]
+                    let vm = data[key];
                     if (vm.hasOwnProperty("events")){
                         vm.events = Object.values(vm.events)
                     }
 
-                    vm.initialVMType = vm.vmType
+                    vm.initialVMType = vm.vmType;
                     //Calculate current VM Type
                     let initType = vmTypes.findIndex(el =>{
                         return el = vm.vmType
-                    })
-                    let delta = 0
+                    });
+                    let delta = 0;
                     vm.events.forEach(e => {
-                        if (e.eventType === "Upgrade") delta++
+                        if (e.eventType === "Upgrade") delta++;
                         else if (e.eventType === "Downgrade") delta--
-                    })
+                    });
 
-                    let newType = initType + delta
+                    let newType = initType + delta;
                     if (newType <=2 && newType >= 0) vm.vmType = vmTypes[newType]
                     
                 })       
@@ -170,7 +170,7 @@ monitor.getVMs = (Event) => {
             resolve(data)
         })
     })
-}
+};
 
 
 //****** Example customerUsageReport
@@ -201,29 +201,29 @@ monitor.allVMUsage = (Event, startTime, endTime) =>{
             listOfVms.forEach(vm => {
                 vm.events.forEach(e => {
                     //find which events are actually within the time frame
-                    let usageEvents= []
-                    if (e.eventTime >= startTime & e.eventTime <= endTime){
+                    let usageEvents= [];
+                    if (e.eventTime >= startTime && e.eventTime <= endTime){
                         usageEvents.push(e)
                     }
-                })
+                });
                 //array for recording all usage cycles for the vm
-                let usageCycles = []
+                let usageCycles = [];
 
                 //iterate through all the events that occured within this time frame
                 for (i = 0; i < usageEvents.length; i++){
 
                     //set the fees based on the vmType during which the usage cycle occurred .
                     let rate = 0;
-                    if (usageEvents[i].vmType == "Large"){
+                    if (usageEvents[i].vmType === "Large"){
                         rate = 0.10; //dollars per minute
-                    } else if (usageEvents[i].vmType == "Ultra-Large") {
+                    } else if (usageEvents[i].vmType === "Ultra-Large") {
                         rate = 0.15;
                     } else {
                         rate = 0.5;
                     }
 
                     //variable used to record the beginning event of a usage cycle
-                    let previousEvent = null
+                    let previousEvent = null;
 
                     //record the beginning of a cycle
                     if (previousEvent == null){
@@ -233,7 +233,7 @@ monitor.allVMUsage = (Event, startTime, endTime) =>{
                     }
 
                     //if the event is a Start
-                    if (usageEvents[i].eventType == "Start"){
+                    if (usageEvents[i].eventType === "Start"){
                         previousEvent = usageEvents[i];
                     }  else {
                         //calculate the cycle duration and multiply by the related rate based on the vm type
@@ -241,16 +241,16 @@ monitor.allVMUsage = (Event, startTime, endTime) =>{
                             duration: (usageEvents[i].eventTime - previousEvent.eventTime)*1000, //convert to seconds
                             vmType: usageEvents[i].vmType,
                             charge: (usageEvents[i].eventTime - previousEvent.eventTime)*1000/60*rate //convert to minutes
-                        }
+                        };
 
                         //add to the list of cycles for this vm within this time period
                         usageCycles.push(cycle);
 
                         //if the event was a stop event, reset the precedent event
-                        if (usageEvents[i].eventType == "Stop"){
-                            previousEvent == null
+                        if (usageEvents[i].eventType === "Stop"){
+                            previousEvent = null
                         }  else {
-                            previousEvent == usageEvents[i];
+                            previousEvent = usageEvents[i];
                         }
                     }
                 }
@@ -258,15 +258,16 @@ monitor.allVMUsage = (Event, startTime, endTime) =>{
                 let vmUsageReport = {
                     vm: vm.vmID,
                     usageCycles: usageCycles
-                }
+                };
 
                 //add this object to the overall usage report for the user
-                customerUsageReport.push(vmUsageReport)
-                resolve(customerUsageReport)
-            })
+                usageReport.push(vmUsageReport);
+            });
+            resolve(usageReport);
+
         });
     })
-}
+};
 
 monitor.singleVMUsage = (Event, startTime, endTime) =>{
     return new Promise((resolve) => {
@@ -275,5 +276,5 @@ monitor.singleVMUsage = (Event, startTime, endTime) =>{
             console.log(listOfVms);
         });
     });
-}
+};
 
