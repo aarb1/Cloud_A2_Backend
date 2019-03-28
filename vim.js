7788bb0bbd43548ccfb0ea1abbc04f9d00b0ac4d
Aayush Bahendwar <aarb1@users.noreply.github.com>
2019-03-27 21:13:29 -0400

    update port to 8080 for compatibility with vm1

--- vim.js
+++ vim.js
@@ -32,7 +32,7 @@ app.use(cors({
  * 
  */
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 
 router.get('/', function (req, res) {
   res.json({ message: 'Hello World!' });
