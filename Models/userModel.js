46dbbf130d81535075efdcc63997de2d67d36f94
jackiengo1 <jngo42@uwo.ca>
2019-03-31 17:58:09 -0400

    Update userModel.js

--- Models/userModel.js
+++ Models/userModel.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-//mongoose.connect('//mongodb://10.0.1.7:20000/cloudDatabase');
-mongoose.connect('mongodb://localhost/cloud');
+mongoose.connect('//mongodb://10.0.1.7:20000/cloudDatabase');
+//mongoose.connect('mongodb://localhost/cloud');
 
 var userSchema = new Schema({
     username: String,
